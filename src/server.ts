f801f19500161dd177229ce9887490ef6b255327
Dmitriy Gamolin <dimademir@gmail.com>
2022-10-06 22:35:49 +0400

    Delete unused user routes

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
 import App from "@/app";
 import AuthRoute from "@routes/auth.route";
 import IndexRoute from "@routes/index.route";
-import UsersRoute from "@routes/users.route";
 import validateEnv from "@utils/validateEnv";
 import CarbonCertificatesRoute from "@routes/carbonCertificates.route";
 
 validateEnv();
 
-const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute(), new CarbonCertificatesRoute()]);
+const app = new App([new IndexRoute(), new AuthRoute(), new CarbonCertificatesRoute()]);
 
 app.listen();
