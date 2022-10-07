b8136adc8eb7e3f2a6c805c0719afd3c1b92485c
Dmitriy Gamolin <dimademir@gmail.com>
2022-10-07 16:40:29 +0400

    Fix logout test

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -5,6 +5,7 @@ import App from '@/app';
 import { CreateUserDto } from '@dtos/users.dto';
 import AuthRoute from '@routes/auth.route';
 import userModel from '@models/users.model';
+import { User } from "../interfaces/users.interface";
 
 afterAll(async () => {
   await mongoose.connection.close()
@@ -57,26 +58,25 @@ describe('Testing Auth', () => {
     });
   });
 
-  // describe('[POST] /logout', () => {
-  //   it('logout Set-Cookie Authorization=; Max-age=0', async () => {
-  //     const userData: User = {
-  //       _id: '60706478aad6c9ad19a31c84',
-  //       email: 'test@email.com',
-  //       password: await bcrypt.hash('q1w2e3r4!', 10),
-  //     };
+  describe('[POST] /logout', () => {
+    it('logout Set-Cookie Authorization=; Max-age=0', async () => {
+      const userData: User = {
+        _id: '60706478aad6c9ad19a31c84',
+        email: 'test@email.com',
+        password: await bcrypt.hash('q1w2e3r4!', 10),
+      };
 
-  //     const authRoute = new AuthRoute();
-  //     const users = authRoute.authController.authService.users;
+      const authRoute = new AuthRoute();
 
-  //     users.findOne = jest.fn().mockReturnValue(userData);
+      userModel.findOne = jest.fn().mockReturnValue(userData);
 
-  //     (mongoose as any).connect = jest.fn();
-  //     const app = new App([authRoute]);
-  //     return request(app.getServer())
-  //       .post(`${authRoute.path}logout`)
-  //       .send(userData)
-  //       .set('Set-Cookie', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ')
-  //       .expect('Set-Cookie', /^Authorization=\; Max-age=0/);
-  //   });
-  // });
+      (mongoose as any).connect = jest.fn();
+      const app = new App([authRoute]);
+      return request(app.getServer())
+        .post(`${authRoute.path}logout`)
+        .set('Set-Cookie', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ')
+        .send(userData)
+        .expect((res) => expect(res.headers['Set-Cookie']).toBeUndefined())
+    });
+  });
 });
